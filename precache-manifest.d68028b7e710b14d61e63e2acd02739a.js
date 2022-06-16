176d3dc5cfb31fd6b4fec02ae15ee10d9fbdfdbc
annasfagiat <annasfagiat41@gmail.com>
2022-06-16 17:26:57 +0700

    Updates

---
+++ precache-manifest.d68028b7e710b14d61e63e2acd02739a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ed2f6bc893612596f5",
+    "url": "/todolist-parkgiyong/static/css/main.d2911e8a.chunk.css"
+  },
+  {
+    "revision": "11ed2f6bc893612596f5",
+    "url": "/todolist-parkgiyong/static/js/main.11ed2f6b.chunk.js"
+  },
+  {
+    "revision": "e8aec68f93cc36643ab7",
+    "url": "/todolist-parkgiyong/static/js/1.e8aec68f.chunk.js"
+  },
+  {
+    "revision": "40776be3512d2b7c5a26",
+    "url": "/todolist-parkgiyong/static/js/runtime~main.40776be3.js"
+  },
+  {
+    "revision": "27e39f5b1856214899351ab9ce4f9dfd",
+    "url": "/todolist-parkgiyong/index.html"
+  }
+];
